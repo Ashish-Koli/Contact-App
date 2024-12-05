261cade9ceeb83772fb32bccd491ecc8a9b746e3
Ashish Koli <justmailashish123@gmail.com>
2024-12-05 22:45:34 +0530

    initial commit

---
+++ src/main/java/com/ashish/contactapi/resource/ContactResource.java
@@ -0,0 +1,54 @@
+package com.ashish.contactapi.resource;
+
+import com.ashish.contactapi.domain.Contact;
+import com.ashish.contactapi.service.ContactService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import static com.ashish.contactapi.constant.Constant.PHOTO_DIRECTORY;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+@RestController
+@RequestMapping("/contacts")
+@RequiredArgsConstructor
+public class ContactResource {
+
+    private final ContactService contactService;
+
+    @PostMapping
+    public ResponseEntity<Contact> createContact(@RequestBody Contact contact){
+        return ResponseEntity.created(URI.create("/contacts/userID")).body(contactService.createContact(contact));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Contact>> getContacts(@RequestParam(value = "page", defaultValue = "0") int page,
+                                                     @RequestParam(value = "size", defaultValue = "10") int size) {
+        return ResponseEntity.ok().body(contactService.getAllContacts(page, size));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Contact> getContact(@PathVariable(value = "id") String id) {
+        return ResponseEntity.ok().body(contactService.getContact(id));
+    }
+
+    @PutMapping("/photo")
+    public ResponseEntity<String> uploadPhoto(@RequestParam("id") String id, @RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok().body(contactService.uploadPhoto(id, file));
+    }
+
+
+    @GetMapping(path = "/image/{filename}", produces = { IMAGE_PNG_VALUE, IMAGE_JPEG_VALUE })
+    public byte[] getPhoto(@PathVariable("filename") String filename) throws IOException {
+        return Files.readAllBytes(Paths.get(PHOTO_DIRECTORY + filename));
+    }
+}
